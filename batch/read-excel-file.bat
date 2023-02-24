234717af1a04a709425a86e32c080865b455ea17
milderlisondra025 <milder.lisondra@staysense.com>
2023-02-23 17:16:27 -0800

    Moved into root directory

--- batch/read-excel-file.bat
+++
@@ -1,8 +0,0 @@
-C:\xampp\php\php.exe C:\Lcg\webscrape\read-excel-file.php Spotlight-Export-09072022.xlsx productsinventoryseptember 2022-09-07 September_2022
-C:\xampp\php\php.exe C:\Lcg\webscrape\read-excel-file.php Spotlight-Export-09112022.xlsx productsinventoryseptember 2022-09-11 September_2022
-C:\xampp\php\php.exe C:\Lcg\webscrape\read-excel-file.php Spotlight-Export-09132022.xlsx productsinventoryseptember 2022-09-13 September_2022
-C:\xampp\php\php.exe C:\Lcg\webscrape\read-excel-file.php Spotlight-Export-09142022.xlsx productsinventoryseptember 2022-09-14 September_2022
-C:\xampp\php\php.exe C:\Lcg\webscrape\read-excel-file.php Spotlight-Export-09152022.xlsx productsinventoryseptember 2022-09-15 September_2022
-C:\xampp\php\php.exe C:\Lcg\webscrape\read-excel-file.php Spotlight-Export-09192022.xlsx productsinventoryseptember 2022-09-19 September_2022
-C:\xampp\php\php.exe C:\Lcg\webscrape\read-excel-file.php Spotlight-Export-09282022.xlsx productsinventoryseptember 2022-09-28 September_2022
-C:\xampp\php\php.exe C:\Lcg\webscrape\read-excel-file.php Spotlight-Export-09302022.xlsx productsinventoryseptember 2022-09-30 September_2022
