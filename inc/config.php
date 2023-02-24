98988ae997fce93a61f730a7c20334d997e0ef1c
milderlisondra025 <milder.lisondra@staysense.com>
2023-02-23 20:00:21 -0800

    New files

---
+++ inc/config.php
@@ -0,0 +1,10 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+date_default_timezone_set("America/Los_Angeles");
+
+require_once "../RedBean/rb.php";
+require_once "../vendor/autoload.php";
+require_once "../inc/spotlight.inc.php";
